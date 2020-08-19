8ef7543b6ac7b53fdeb2376de0a71f226414fa22
SamSerra7 <samsserranoguerra7@gmail.com>
2020-08-18 23:40:22 -0600

    Asignación 1, listarProductos - Entidad,Datos,Negocio,API Controller

---
+++ BackEndVentR/BackEndVentR/Dato/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dato
+{
+    class Conexion
+    {
+        public NpgsqlConnection GetConexion()
+        {
+            /*CREDENCIALES SAMUEL*/
+            var cs = "Host=localhost;Port=5433;Username=samuel;Password=s.1234;Database=VentR";
+
+            return new NpgsqlConnection(cs);
+        }
+    }
+}
---
+++ BackEndVentR/BackEndVentR/Dato/ProductoDatos.cs
@@ -0,0 +1,56 @@
+﻿using Entidad;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Text;
+
+namespace Dato
+{
+    /// <summary>
+    /// Clase que se conecta con la tabla de Productos en lla base de datos
+    /// </summary>
+    public class ProductoDatos
+    {
+
+        ///Variables globales
+        private Conexion conexion = new Conexion();
+
+        /// <summary>
+        /// Samuel Serrano 
+        /// Método que obtiene todos los productos de la BD
+        /// </summary>
+        /// <returns>Products List</returns>
+        public List<Producto> listarProductos()
+        {
+
+            List<Producto> productos = new List<Producto>();
+
+            using(NpgsqlConnection con = conexion.GetConexion())
+            {
+                con.Open();
+                string sql = "SELECT id_producto,nombre,precio,url_img,detalle,activo,cantidad FROM products.\"Producto\"";
+                
+                using(var command = new NpgsqlCommand(sql, con))
+                {
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            productos.Add( new Producto(reader.GetInt32(0), reader.GetString(1),
+                                                            reader.GetDecimal(2), reader.GetString(3), 
+                                                                reader.GetString(4),reader.GetBoolean(5),
+                                                                    reader.GetInt32(6))
+                            );
+                        }
+
+                    }
+                }
+            }
+            return productos;
+        }
+
+
+
+    }
+}
