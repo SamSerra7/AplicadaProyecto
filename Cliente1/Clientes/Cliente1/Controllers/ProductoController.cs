b814f0a98fb41b0224992bb6507e1a836416121b
Jahanel <jahanrive3131@gmail.com>
2020-08-20 15:17:23 -0600

    Creacion de proyecto, se cambio porque la estructura del otro estaba mala, se hizo lista de productos, BD y Clase producto

---
+++ Cliente1/Clientes/Cliente1/Controllers/ProductoController.cs
@@ -0,0 +1,54 @@
+﻿using Cliente1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cliente1.Controllers
+{
+    public class ProductoController : Controller
+    {
+
+        private PriductoData pd;
+
+        // GET: Producto
+        public ActionResult ObtenerLista()
+        {
+            pd = new PriductoData();
+            ModelState.Clear();
+            return View(pd.obtenerProductos());
+        }
+
+        [HttpGet]
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(Producto producto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    pd = new PriductoData();
+                    if (pd.Add(producto))
+                    {
+                        ViewBag.Mensagen = " ingresado con exito ";
+                    }
+
+                }
+                return View();
+            }
+            catch (Exception) 
+            {
+                return View("ObtenerLista");
+            }
+        }
+
+
+    }
+}
---
+++ Cliente1/Clientes/Cliente1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cliente1.MvcApplication" Language="C#" %>
