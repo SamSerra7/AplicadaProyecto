d6fbbfddb641c6282a538866291fecafc7d216bb
maikelmatamoros <37597397+maikelmatamoros@users.noreply.github.com>
2020-08-20 04:07:05 -0600

    Se agregó la entidad Usuario y la entidad Rol

---
+++ BackEndVentR/BackEndVentR/Entidad/Rol.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidad
+{
+    class Rol
+    {
+        public int Id_Rol { get; set; }
+        public string Nombre { get; set; }
+
+        public Rol()
+        {
+        }
+
+        public Rol(int id_rol,string nombre)
+        {
+            this.Id_Rol = id_rol;
+            this.Nombre = nombre;
+        }
+
+    }
+}
