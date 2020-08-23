f098ffce5331132483f6958d8ccb27dd1c475049
SamSerra7 <samsserranoguerra7@gmail.com>
2020-08-23 01:41:24 -0600

    Asignación 2 = Llave (entidad,data,negocio,controller)

---
+++ BackEndVentR/BackEndVentR/BackEndVentR/Controllers/LlaveController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entidad;
+using Microsoft.AspNetCore.Mvc;
+using Negocio;
+
+namespace BackEndVentR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LlaveController : ControllerBase
+    {
+        ///Variable capa negocios
+        private LlaveNegocio llaveNegocio = new LlaveNegocio();
+
+
+        
+        // GET api/<LlaveController>/5
+        [HttpGet("{idProveedor}")]
+        public bool tieneLlaveActiva(int idProveedor)
+        {
+            return llaveNegocio.tieneLlaveActiva(idProveedor);
+        }
+
+
+        // POST api/<LlaveController>
+        [HttpPost]
+        public void crearLlave([FromBody] int idProveedor)
+        {
+            llaveNegocio.crearLlave(idProveedor);
+        }
+
+       
+
+    }
+}
---
+++ BackEndVentR/BackEndVentR/Negocio/LlaveNegocio.cs
@@ -0,0 +1,36 @@
+﻿using Dato;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Negocio
+{
+    public class LlaveNegocio
+    {
+        //Variable Capa Datos
+        private LlaveDatos llaveDatos = new LlaveDatos();
+
+        /// <summary>
+        /// Samuel Serrano Guerra
+        /// Método que verifica si un usuario tiene una llave activa
+        /// </summary>
+        /// <param name="idProveedor"></param>
+        /// <returns>variable booleana</returns>
+        public bool tieneLlaveActiva(int idProveedor)
+        {
+            return idProveedor > 0 ? llaveDatos.tieneLlaveActiva(idProveedor) : false;
+        }
+
+
+        /// <summary>
+        /// Samuel Serrano Guerra
+        /// Método que crea una llave para un proveedor en específico
+        /// </summary>
+        /// <param name="idProveedor"></param>
+        public void crearLlave(int idProveedor)
+        {
+            if(idProveedor <= 0) throw new Exception("Proveedor inválido");
+            llaveDatos.crearLlave(idProveedor);
+        }
+    }
+}
