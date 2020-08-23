a3ec61f6737aeb4f4d688491325f57c9f0686a36
maikelmatamoros <37597397+maikelmatamoros@users.noreply.github.com>
2020-08-23 03:38:45 -0600

    Creacion del Dato, Negocio y Controller de Rol
    Se creo el metodo ListaRoles en las diferentes capas

---
+++ BackEndVentR/BackEndVentR/BackEndVentR/Controllers/RolController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Negocio;
+using Entidad;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEndVentR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolController : ControllerBase
+    {
+
+        private RolNegocio rolNegocio = new RolNegocio();
+        // GET: api/<RolController>
+        [HttpGet]
+        public IEnumerable<Rol> ListarRoles()
+        {
+            return rolNegocio.listarRoles();
+        }
+
+        // GET api/<RolController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RolController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RolController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RolController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BackEndVentR/BackEndVentR/Negocio/RolNegocio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dato;
+using Entidad;
+
+namespace Negocio
+{
+    public class RolNegocio
+    {
+
+        private RolDatos rolDatos = new RolDatos();
+
+        public IEnumerable<Rol> listarRoles()
+        {
+            List<Rol> roles = rolDatos.listarRoles();
+
+            return roles;
+
+        }
+
+    }
+}
