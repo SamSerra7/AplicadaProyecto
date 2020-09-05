742d517b62b8a58ea96442a0ca62f8d9fb4967fb
Jahanel <jahanrive3131@gmail.com>
2020-09-05 13:01:42 -0600

    Generar Llave y disminuir cantidad

---
+++ Cliente1/Cliente1/Constante/Constantes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cliente1.Constante
+{
+    public class Constantes
+    {
+        public const string URLParaRecibirLlave = "http://localhost:59292/api/Llave";
+
+        public const string URLParaDisminuirCantidadProducto = "http://localhost:62464/api/usuario";
+    }
+}
---
+++ Cliente1/Cliente1/EnvioDato/ReciboDatoLlave.cs
@@ -0,0 +1,31 @@
+﻿using Cliente1.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace Cliente1.EnvioDato
+{
+    public class ReciboDatoLlave
+    {
+
+        public async System.Threading.Tasks.Task<Llave> recibirLlave()
+        {
+            using (var httpClient = new HttpClient())
+            {
+
+                string url = string.Concat(Constante.Constantes.URLParaRecibirLlave, "/metodoControladorParaREcibirLlave/" + 2);
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+
+                    var respuesta = JsonConvert.DeserializeObject<Llave>(apiResponse);
+                    return respuesta;
+                }
+            }
+        }
+      
+    }
+}
